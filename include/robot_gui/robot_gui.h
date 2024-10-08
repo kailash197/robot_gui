0c8f8077b3ae392396b17e1dea2f3190749d3f89
Kailash Khadka <k.khadka343@gmail.com>
2024-10-08 19:44:22 +0000

    Teleoperation Buttons

--- include/robot_gui/robot_gui.h
+++ include/robot_gui/robot_gui.h
@@ -1,10 +1,9 @@
 #pragma once
-
-#include "robotinfo_msgs/RobotInfo10Fields.h"
-#include "ros/node_handle.h"
 #define CVUI_IMPLEMENTATION
 #include "cvui.h"
-#include "std_msgs/Float64.h"
+#include "geometry_msgs/Twist.h"
+#include "robotinfo_msgs/RobotInfo10Fields.h"
+#include "ros/node_handle.h"
 #include <opencv2/opencv.hpp>
 #include <ros/ros.h>
 
@@ -13,7 +12,7 @@ public:
   RobotGUI(ros::NodeHandle *nh);
   void run();
   void robot_info_display(cv::Mat &frame);
-  void teleop_buttons();
+  void teleop_buttons(cv::Mat &frame);
   void current_velocity();
   void robot_position_odom();
   void distance_travelled_service();
@@ -21,8 +20,12 @@ public:
 private:
   ros::NodeHandle *nh_;
   ros::Subscriber sub_;
-  robotinfo_msgs::RobotInfo10Fields message;
-  std::string topic_name;
-  void msgCallback(const robotinfo_msgs::RobotInfo10Fields::ConstPtr &msg);
+  robotinfo_msgs::RobotInfo10Fields robotinfo_message;
+  ros::Publisher pub_;
+  ros::Subscriber velocity_sub_;
+  geometry_msgs::Twist twist_message;
+  std::string info_topic_name, vel_topic_name;
+  void msgCallback(const robotinfo_msgs::RobotInfo10Fields::ConstPtr &info_msg);
+  void velMsgCallback(const geometry_msgs::Twist::ConstPtr &twist_msg);
   const std::string WINDOW_NAME = "ROS ROBOT GUI";
 };
--- src/robot_gui.cpp
+++ src/robot_gui.cpp
@@ -1,19 +1,29 @@
 #include "../include/robot_gui/robot_gui.h"
+#include "geometry_msgs/Twist.h"
 #include "robotinfo_msgs/RobotInfo10Fields.h"
 #include "ros/node_handle.h"
 
 RobotGUI::RobotGUI(ros::NodeHandle *node_handle) {
   this->nh_ = node_handle;
-  topic_name = "robot_info";
+  info_topic_name = "robot_info";
+  vel_topic_name = "cmd_vel";
   sub_ = nh_->subscribe<robotinfo_msgs::RobotInfo10Fields>(
-      topic_name, 20, &RobotGUI::msgCallback, this);
+      info_topic_name, 20, &RobotGUI::msgCallback, this);
+  velocity_sub_ = nh_->subscribe<geometry_msgs::Twist>(
+      vel_topic_name, 20, &RobotGUI::velMsgCallback, this);
+  pub_ = nh_->advertise<geometry_msgs::Twist>(vel_topic_name, 100);
   ROS_INFO("RobotGUI Node created.");
+}
+
+void RobotGUI::velMsgCallback(const geometry_msgs::Twist::ConstPtr &twist_msg) {
+  twist_message = *twist_msg;
+  ROS_DEBUG("Twist Message Received.");
 }
 
 void RobotGUI::msgCallback(
     const robotinfo_msgs::RobotInfo10Fields::ConstPtr &msg) {
-  message = *msg;
-  ROS_INFO("Robot Info Message Received.");
+  robotinfo_message = *msg;
+  ROS_DEBUG("Robot Info Message Received.");
 }
 
 void RobotGUI::robot_info_display(cv::Mat &frame) {
@@ -25,19 +35,69 @@ void RobotGUI::robot_info_display(cv::Mat &frame) {
   float font_s = 0.4;
 
   std::vector<std::string> data_fields = {
-      message.data_field_01, message.data_field_02, message.data_field_03,
-      message.data_field_04, message.data_field_05, message.data_field_06,
-      message.data_field_07, message.data_field_08, message.data_field_09,
-      message.data_field_10};
+      robotinfo_message.data_field_01, robotinfo_message.data_field_02,
+      robotinfo_message.data_field_03, robotinfo_message.data_field_04,
+      robotinfo_message.data_field_05, robotinfo_message.data_field_06,
+      robotinfo_message.data_field_07, robotinfo_message.data_field_08,
+      robotinfo_message.data_field_09, robotinfo_message.data_field_10};
 
   cvui::window(frame, win_posx, win_posy, win_width, win_height,
-               "Topic: " + topic_name);
+               "Topic: " + info_topic_name);
 
   for (int i = 0; i < 10; i++) {
     cvui::printf(frame, posx, posy + 20 * i, font_s, white,
                  data_fields[i].c_str());
   }
 }
+
+void RobotGUI::teleop_buttons(cv::Mat &frame) {
+  // parameters
+  int width = 90, height = 30;
+  float font_s = 0.5;
+  int for_posx = 160, for_posy = 230;
+  int stp_posx = 160, stp_posy = 270;
+  int bac_posx = 160, bac_posy = 310;
+  int lft_posx = 60, lft_posy = 270;
+  int rgt_posx = 260, rgt_posy = 270;
+
+  if (cvui::button(frame, for_posx, for_posy, width, height, "Forward")) {
+    if (twist_message.linear.x < 1.0) {
+      twist_message.linear.x += 0.1;
+    } else {
+      twist_message.linear.x = 1.0;
+    }
+  }
+
+  if (cvui::button(frame, stp_posx, stp_posy, width, height, "Stop")) {
+    twist_message.linear.x = 0.0;
+    twist_message.angular.z = 0.0;
+  }
+
+  if (cvui::button(frame, bac_posx, bac_posy, width, height, "Backward")) {
+    if (twist_message.linear.x > -1.0) {
+      twist_message.linear.x -= 0.1;
+    } else {
+      twist_message.linear.x = -1.0;
+    }
+  }
+
+  if (cvui::button(frame, lft_posx, lft_posy, width, height, "Left")) {
+    if (twist_message.angular.z < 1.0) {
+      twist_message.angular.z += 0.1;
+    } else {
+      twist_message.angular.z = 1.0;
+    }
+  }
+  if (cvui::button(frame, rgt_posx, rgt_posy, width, height, "Right")) {
+    if (twist_message.angular.z > -1.0) {
+      twist_message.angular.z -= 0.1;
+    } else {
+      twist_message.angular.z = -1.0;
+    }
+  }
+  pub_.publish(twist_message);
+}
+
 void RobotGUI::run() {
   // canvas for drawing the GUI height x width
   cv::Mat frame = cv::Mat(700, 400, CV_8UC3);
@@ -52,11 +112,9 @@ void RobotGUI::run() {
 
     // general info display
     robot_info_display(frame);
+    teleop_buttons(frame);
 
-    // Update cvui internal stuff
     cvui::update();
-
-    // Show everything on the screen
     cv::imshow(WINDOW_NAME, frame);
 
     // Check if ESC key was pressed
